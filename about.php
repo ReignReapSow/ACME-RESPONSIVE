474e564db9d49474231a095a0b4d23b2ee3bbf1c
Reign Westry <18231550+ReignReapSow@users.noreply.github.com>
2018-10-24 17:24:55 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Reign
+ * Date: 4/24/2017
+ * Time: 12:30 AM
+ */
+
+?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<!--SEO Optimization-->
+	<meta name="description" content="Affordable and professional web design">
+	<meta name="keywords" content="Affordable, professional web design, affordable">
+	<meta name="author" content="Reign Westry">
+	<!--SEO-->
+	<title>ACME Web Design| Welcome</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<section>
+	<header>
+		<div class="container">
+			<div id="branding">
+				<h1><span class="highlight">ACME</span> Web Design</h1>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a> </li>
+					<li class="current"><a href="#">About</a> </li>
+					<li><a href="#">Services</a> </li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+</section>
+</section id="newsletter">
+<div class="container">
+	<h1>Subscribe To Our Newsletter</h1>
+	<form>
+		<input type="email" placeholder="Enter Email...">
+		<button type="submit" class="btn-One">Subscribe</button>
+	</form>
+</div>
+<section id="main">
+	<div class="container">
+	</div>
+	<footer>
+		<p>Acme Web Design, Copyright &copy; 2017</p>
+	</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Reign
+ * Date: 4/24/2017
+ * Time: 12:30 AM
+ */
+
+?>
+<!doctype html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width">
+            <!--SEO Optimization-->
+            <meta name="description" content="Affordable and professional web design">
+            <meta name="keywords" content="Affordable, professional web design, affordable">
+            <meta name="author" content="Reign Westry">
+            <!--SEO-->
+            <title>ACME Web Design| Welcome</title>
+            <link rel="stylesheet" href="css/style.css">
+        </head>
+        <body>
+            <section>
+                <header>
+                    <div class="container">
+                        <div id="branding">
+                            <h1><span class="highlight">ACME</span> Web Design</h1>
+                        </div>
+                        <nav>
+                            <ul>
+                                <li class="current"><a href="#">Home</a> </li>
+                                <li><a href="#">About</a> </li>
+                                <li><a href="#">Services</a> </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
+
+            </section>
+            <section id="showcase">
+                <div class="container">
+                    <h1>Affordable Professional Web Designs</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis in libero sed blandit. Phasellus dapibus turpis eu quam congue ullamcorper. </p>
+                </div>
+            </section id="newsletter">
+                <div class="container">
+                    <h1>Subscribe To Our Newsletter</h1>
+                    <form>
+                        <input type="email" placeholder="Enter Email...">
+                        <button type="submit" class="btn-One">Subscribe</button>
+                    </form>
+                </div>
+            <section id="boxes">
+                <div class="container">
+                    <div class="box">
+                        <img src="./imgs/logo_html.png">
+                        <h3>HTML5 Markup</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis in libero sed blandit. </p>
+                    </div>
+                    <div class="box">
+                        <img src="./imgs/logo_css.png">
+                        <h3>CSS3 Styling</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis in libero sed blandit. </p>
+                    </div>
+                    <div class="box">
+                        <img src="./imgs/logo_brush.png">
+                        <h3>Graphic Design</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis in libero sed blandit. </p>
+                    </div>
+                </div>
+            <footer>
+                <p>Acme Web Design, Copyright &copy; 2017</p>
+            </footer>
+        </body>
+    </html>
